1463728ce2e0c7bd779b1d8d678a805d82344f8b
KrisAff84 <krisaff@gmail.com>
2024-07-10 19:31:47 -0500

    Adds environment variables in doc string of authorizer function

--- api_authorizer.py
+++ api_authorizer.py
@@ -1,9 +1,6 @@
-import json
+
 
 def lambda_handler(event, context):
-    
-    # Log the full event for debugging
-    print("Full event received:", json.dumps(event))
 
     headers = event.get('headers', {})
     
